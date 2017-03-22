65e93ad1ff118b4f3f2f5b62d4f17ba7bc51a9a8
padhupradheep <padhupradheep@gmail.com>
2017-03-22 13:29:42 +0100

    The updated

--- left-right.py
+++ left-right.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python
 import rospy
+
+
 from geometry_msgs.msg import Twist
 
 
 
 def left_right():
-	#global velocity_publisher
-	#twist = Twist()
-	st1=raw_input("Enter the string")
-	if st1=="left":
-		print "left"
-		return 10
-        else:
-                return 0
- 
-	#velocity_publisher.publish(twist)
-    
-    
+	global velocity_publisher
+	twist = Twist()
+	rospy.init_node('leftright')    
+  
+    	velocity_publisher = rospy.Publisher('/cmd_vel', Twist,queue_size=10)
+	con="y"
+	while(con=="y"):
+		con=raw_input("Do you want to continue y/n ")
+		st1=raw_input("Enter the string")
+		if st1=="left":
+			twist.linear.x=1
+			twist.linear.y = 0.0
+			print "left"
 
-def main():
-    #global velocity_publisher
-    x_lin=left_right()
-    rospy.init_node('leftright') 
-    twist.linear.x=x_lin
-    velocity_publisher =  rospy.Publisher('/cmd_vel', Twist)
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        rospy.logwarn("Running the node, time {}".format(rospy.get_time()))
-        rate.sleep()
-    pass
+		elif st1=="right":
+			twist.linear.x=-0.1
+			twist.linear.y=0.0
+			print "rigt"
+		elif st1=="stop":
+			twist.linear.x=0.0
+	
+		velocity_publisher.publish(twist)
+	
+
+
+    
+    
+  
+
 
 if __name__ == '__main__':
-   main()
+   left_right()
